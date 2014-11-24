d23ed720868d59805880f3b000aa8bb05d844543
Giannis <giannisf@yandex.ru>
2014-11-24 19:11:28 +0200

    tabination fix works like a charm

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,30 @@
 $(function() {
 
+    $("ul.nav-tabs a").click(function (e) {
+        e.preventDefault();
+
+        $(this).tab('show');
+
+        var selector = '.' + e.target.innerHTML.toLowerCase();
+
+        var tabs = $('.metric-tabs').find(selector);
+
+        var others = $('.metric-tabs').find('div').not(selector);
+
+
+        console.log(others)
+        _.each(others,function(item) {
+            var temp = $(item);
+
+            if (temp.hasClass('active')) {
+                temp.removeClass('active')
+            }
+
+
+        });
+        tabs.addClass('active');
+
+    });
 
 
 });
